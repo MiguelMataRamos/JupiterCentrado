eff245d35cccfc1a0479d2310386afee6be47385
EAG <mataramosmiguel@gmail.com>
2023-10-18 19:06:49 +0200

    jupiter y la luna

--- app/src/main/java/com/example/jupitercentrado/MainActivity.kt
+++ app/src/main/java/com/example/jupitercentrado/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.jupitercentrado
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val jupiter = findViewById<ImageView>(R.id.jupiter)
+        val luna = findViewById<ImageView>(R.id.luna)
+        val texto = findViewById<TextView>(R.id.texto)
+
+        jupiter.setOnClickListener {
+            texto.text = "JUPITER"
+        }
+        luna.setOnClickListener {
+            texto.text = "LUNA"
+        }
+
+
     }
 }
